2eb16733fd1f653d3e7f4983a1fbfd6b67cb0c34
Dawei95271 <dawei95271@qq.com>
2023-02-11 15:40:05 +0800

    提交

--- heima-leadnews-service/heima-leadnews-wemedia/src/main/java/com/heima/wemedia/service/WmSensitiveService.java
+++
@@ -1,8 +0,0 @@
-package com.heima.wemedia.service;
-
-
-import com.baomidou.mybatisplus.extension.service.IService;
-import com.heima.model.sensitive.dtos.WmSensitive;
-
-public interface WmSensitiveService extends IService<WmSensitive> {
-}
